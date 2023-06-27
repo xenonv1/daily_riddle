3cbdee2695e22ce9575ead22b156697aeb773ef4
xenonv1 <82435371+xenonv1@users.noreply.github.com>
2023-06-27 23:19:53 +0200

    initialized app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+export default function RiddleApp() {
+  const [riddle, setRiddle] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [showAnswer, toggleShowAnswer] = useState(false);
+
+  useEffect(() => {
+    axios.get("https://riddles-api.vercel.app/random").then((res) => {
+      setRiddle(res.data.riddle);
+      setAnswer(res.data.answer);
+    });
+  }, []);
+
+  function revealAnswer() {
+    toggleShowAnswer(!showAnswer);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="main-container">
+      <div className="content-container">
+        <h1>
+          Welcome to <span className="secondary-font">Riddler</span>! Have fun
+          solving your daily riddle!
+        </h1>
+        <h2>{riddle}</h2>
+        <button onClick={revealAnswer}>
+          {!showAnswer ? "Reveal answer" : "Hide answer"}
+        </button>
+        {showAnswer ? (
+          <h2>{answer}</h2>
+        ) : (
+          <p className="secondary-font hint">
+            Click the button above to reveal the answer to this riddle :)
+          </p>
+        )}
+      </div>
+      <div className="credits-container">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Made with by{" "}
+          <a
+            href="https://github.com/xenonv1/"
+            target="_blank"
+          >
+            nb
+          </a>{" "}
+          with ❤️
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          using{" "}
+          <a
+            href="https://riddles-api.vercel.app/"
+            target="_blank"
+          >
+            riddles api
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://github.com/nkilm"
+            target="_blank"
+          >
+            Nikhil Mohite
+          </a>
+        </p>
+        <p>Thank you for providing the riddles!</p>
+      </div>
     </div>
   );
 }
-
-export default App;
